b5f5bcae0d8ba9e04dce84a2e0d15d6916f42b7e
Hamza Ali <me@hamzantal.pw>
2022-01-21 06:19:21 +0700

    Implement typedef lsp command
    Co-Authored-By: Chan Wen Xu <lutherchanpublic@gmail.com>

--- lsp/typedef.go
+++ lsp/typedef.go
@@ -2,18 +2,20 @@ package lsp
 
 import (
 	"context"
+	"errors"
 	"fmt"
 	"go/ast"
 	"go/token"
-	"log"
+	"go/types"
 	"strconv"
-	"strings"
 
 	"github.com/gunk/gunkls/lsp/loader"
 	"go.lsp.dev/jsonrpc2"
 	"go.lsp.dev/protocol"
 	"go.lsp.dev/uri"
 )
+
+var invalidType = errors.New("can only go to definition on struct or enum types")
 
 func (l *LSP) Goto(ctx context.Context, params protocol.DefinitionParams, reply jsonrpc2.Replier) {
 	file := params.TextDocument.URI.Filename()
@@ -51,143 +53,135 @@ func (l *LSP) Goto(ctx context.Context, params protocol.DefinitionParams, reply 
 	pos.Character++
 	pos.Line++
 
-	var done bool
-	ast.Inspect(f, func(node ast.Node) bool {
-		if done {
-			return false
-		}
+	type bailout struct{}
 
+	defer func() {
+		x := recover()
+		if x == nil {
+			return
+		}
+		if _, ok := x.(bailout); ok {
+			return
+		}
+		panic(x)
+	}()
+
+	var foundTyp bool
+	ast.Inspect(f, func(node ast.Node) bool {
 		switch node := node.(type) {
 		default:
 			return false
-		case *ast.File, *ast.GenDecl, *ast.TypeSpec, *ast.StructType, *ast.InterfaceType, *ast.FieldList, *ast.FuncType:
+		case *ast.File, *ast.GenDecl, *ast.TypeSpec, *ast.FieldList, *ast.Field, *ast.StructType, *ast.InterfaceType:
+			return contains(l.loader.Fset, node, pos)
+		case *ast.ArrayType, *ast.FuncType, *ast.ChanType, *ast.MapType:
+			if !contains(l.loader.Fset, node, pos) {
+				return false
+			}
+			// Make a note that we are inside these types so we can notify the
+			// user that they should place their cursor on the identifier.
+			foundTyp = true
 			return true
 		case *ast.ImportSpec:
 			if !contains(l.loader.Fset, node, pos) {
 				return false
 			}
-			done = true
-			path, _ := strconv.Unquote(node.Path.Value)
-			pkgs, err := l.loader.Load(path)
-			if err != nil || len(pkgs) > 1 {
-				reply(ctx, nil,
-					fmt.Errorf("unexpected error loading %q: %v", path, err))
+			l.gotoImport(ctx, node, reply)
+			panic(bailout{})
+		case *ast.SelectorExpr, *ast.Ident:
+			if !contains(l.loader.Fset, node, pos) {
 				return false
 			}
-			if len(pkgs) == 0 {
-				l.msg(ctx, protocol.MessageTypeError,
-					fmt.Sprintf("no gunk files in package %s", node.Path.Value))
-				return false
-			}
-
-			var files []protocol.Location
-			for _, v := range pkgs[0].GunkFiles {
-				files = append(files, protocol.Location{
-					URI: uri.File(v),
-					Range: protocol.Range{
-						Start: protocol.Position{
-							Line:      0,
-							Character: 0,
-						},
-						End: protocol.Position{
-							Line:      0,
-							Character: 0,
-						},
-					},
-				})
-			}
-			reply(ctx, files, nil)
-		case *ast.Field:
-			if _, ok := node.Type.(*ast.FuncType); ok {
-				return true
-			}
-			if !contains(l.loader.Fset, node.Type, pos) {
-				return false
-			}
-			done = true
-			if len(node.Names) == 0 {
-				reply(ctx, nil,
-					fmt.Errorf("no nodes found for %v", node))
-				return false
-			}
-			name := pkg.TypesInfo.TypeOf(node.Names[0])
-			if name == nil {
-				reply(ctx, nil,
-					fmt.Errorf("no type found for %v", node))
-				return false
-			}
-			str := name.String()
-			for strings.HasPrefix(str, "[]") {
-				str = str[2:]
-			}
-			dot := strings.LastIndexByte(str, '.')
-			if dot == -1 {
-				reply(ctx, nil, fmt.Errorf("type is builtin"))
-				// builtin
-				return false
-			}
-			pkgName := str[:dot]
-			typeName := str[dot+1:]
-
-			if pkgName != pkg.Package.ID {
-				pkgs, err := l.loader.Load(pkgName)
-				if err != nil {
-					reply(ctx, nil, fmt.Errorf("unexpected error loading %q: %v", pkgName, err))
-					return false
-				}
-				if len(pkgs) != 1 {
-					reply(ctx, nil, fmt.Errorf("unexpected error loading %q: %v", pkgName, err))
-					return false
-				}
-				pkg = pkgs[0]
-			}
-			var done bool
-			for i, f := range pkg.GunkSyntax {
-				path := pkg.GunkFiles[i]
-				log.Println("checking", path)
-				ast.Inspect(f, func(n ast.Node) bool {
-					if done {
-						return false
-					}
-					switch n := n.(type) {
-					default:
-						return false
-					case *ast.File, *ast.GenDecl:
-						return true
-					case *ast.TypeSpec:
-						position := l.loader.Fset.Position(n.Pos())
-						pos := protocol.Position{
-							Line:      uint32(position.Line) - 1,
-							Character: uint32(position.Column) - 1,
-						}
-						if n.Name.Name == typeName {
-							reply(ctx, protocol.Location{
-								URI: uri.File(path),
-								Range: protocol.Range{
-									Start: pos,
-									End:   pos,
-								},
-							}, nil)
-							return false
-						}
-					}
-					return false
-				})
-				if done {
-					break
-				}
-			}
+			// node must be an expression as it can only be selector or identifier.
+			n := node.(ast.Expr)
+			l.gotoType(ctx, pkg, n, reply)
+			panic(bailout{})
 		}
-		return false
 	})
+
+	if foundTyp {
+		reply(ctx, nil, invalidType)
+		return
+	}
+	// Not a valid location to go to definition at.
+	reply(ctx, nil, nil)
+}
+
+// gotoImport handles goto requests when the cursor is on an import path.
+func (l *LSP) gotoImport(ctx context.Context, spec *ast.ImportSpec, reply jsonrpc2.Replier) {
+	// Load the package specified.
+	path, _ := strconv.Unquote(spec.Path.Value)
+	pkgs, err := l.loader.Load(path)
+	if err != nil || len(pkgs) > 1 {
+		reply(ctx, nil, fmt.Errorf("unexpected error loading %q: %v", path, err))
+		return
+	}
+	if len(pkgs) == 0 {
+		reply(ctx, nil, fmt.Errorf("no gunk files in package %s", spec.Path.Value))
+		return
+	}
+	// Create the list of files to reply with.
+	pkg := pkgs[0]
+	files := make([]protocol.Location, 0, len(pkg.GunkFiles))
+	for _, v := range pkg.GunkFiles {
+		files = append(files, protocol.Location{
+			URI: uri.File(v),
+		})
+	}
+	reply(ctx, files, nil)
+}
+
+// gotoIdent handles goto requests when the cursor is on a type.
+func (l *LSP) gotoType(ctx context.Context, pkg *loader.GunkPackage, expr ast.Expr, reply jsonrpc2.Replier) {
+	typAndValue := pkg.TypesInfo.Types[expr]
+	if !typAndValue.IsType() {
+		// Not a type. Ignore.
+		reply(ctx, nil, nil)
+		return
+	}
+	typ := typAndValue.Type
+	switch typ := typ.(type) {
+	default:
+		reply(ctx, nil, fmt.Errorf("unknown type of type: %T", typ))
+		return
+	case *types.Basic:
+		reply(ctx, nil, invalidType)
+		return
+	case *types.Named:
+		pos := l.loader.Fset.Position(typ.Obj().Pos())
+		if !pos.IsValid() {
+			reply(ctx, nil, invalidType)
+			return
+		}
+		loc := protocol.Location{
+			URI: uri.File(pos.Filename),
+			Range: protocol.Range{
+				Start: protocol.Position{
+					Line:      uint32(pos.Line - 1),
+					Character: uint32(pos.Column - 1),
+				},
+				End: protocol.Position{
+					Line:      uint32(pos.Line - 1),
+					Character: uint32(pos.Column - 1),
+				},
+			},
+		}
+		reply(ctx, []protocol.Location{loc}, nil)
+		return
+	}
 }
 
 func contains(fset *token.FileSet, node ast.Node, pos protocol.Position) bool {
 	start := fset.Position(node.Pos())
 	end := fset.Position(node.End())
 
-	return int(pos.Line) >= start.Line &&
-		int(pos.Line) <= end.Line &&
-		int(pos.Character) >= start.Column &&
-		int(pos.Character) <= end.Column
+	if int(pos.Line) < start.Line || int(pos.Line) > end.Line {
+		return false
+	}
+	if int(pos.Line) == start.Line && int(pos.Character) < start.Column {
+		return false
+	}
+	if int(pos.Line) == end.Line && int(pos.Character) > end.Column {
+		return false
+	}
+	return true
 }
